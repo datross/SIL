45deb80e370f37e0b45c900878ff88822f48bd44
datross <datross@openmailbox.org>
2016-12-28 09:31:27 +0100

    added get_last() in Function_pool

--- src/src/function_pool.cpp
+++ src/src/function_pool.cpp
@@ -10,6 +10,7 @@ Function_ptr Function_pool::add(std::string name,
                      vartype::variable_type return_type, 
                      std::vector<Function_parameter> parameters) {
         functions[name] = Function_ptr(new Function(name, return_type, parameters));
+        last = functions[name];
         return functions[name];
 }
 
@@ -17,3 +18,6 @@ Function_ptr Function_pool::get(std::string name) {
     return functions[name];
 }
 
+Function_ptr Function_pool::get_last() const {
+    return last;
+}
--- src/src/function_pool.h
+++ src/src/function_pool.h
@@ -12,9 +12,9 @@ namespace function {
 class Function_pool {
 private:
     std::map<std::string, Function_ptr> functions;
+    Function_ptr last;
 public:
     Function_pool();
-//     ~Function_pool();
     
     /* Create a function with given parameters, and a 
        shared_ptr on it. */
@@ -24,6 +24,9 @@ public:
     
     /* Returns a shared_ptr on the function with the given name. */
     Function_ptr get(std::string name);
+    
+    /* Returns  shared_ptr on the last function added to the pool (NULL if no function has been added yet). */
+    Function_ptr get_last() const;
 };
 
 }
